b02777d7940156a7257473bc63868d4928c4e417
NVasilev8 <nvasilev2121@gmail.com>
2026-02-04 20:26:34 +0200

    Admin dashboard modern redesign: Updated topbar, sidebar and global styles with gradients, animations and modern UI

--- admin/src/index.tsx
+++ admin/src/index.tsx
@@ -4,6 +4,8 @@ import App from "./App";
 import { store, persistor } from "./redux/store";
 import { PersistGate } from "redux-persist/es/integration/react";
 import { Provider } from "react-redux";
+import "./admin-global.css";
+import "./admin-components.css";
 
 ReactDOM.render(
   <React.StrictMode>
